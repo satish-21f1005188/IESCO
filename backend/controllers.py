68daea27ad436accb2e05e54e5d4badddfca6d82
satish-21f1005188 <21f1005188@ds.study.iitm.ac.in>
2024-07-05 19:25:42 +0530

    done login and signup controls with db models

--- backend/controllers.py
+++ backend/controllers.py
@@ -1,10 +1,10 @@
 from flask import Flask,render_template,request
 
 from flask import current_app as app # current running app
-from models import *
+from .models import *
 @app.route("/")
 def home():
-    return "<h2>Fuck you ,im routing here"
+    return render_template("login.html")
 
 @app.route("/login",methods=["GET",'POST'])
 def user_login():
@@ -12,14 +12,74 @@ def user_login():
     if request.method == "POST":
         uname = request.form.get("uname")
         pwd = request.form.get("pwd")
-        usr = table_1.query.filter_by(user_name = uname,
+        usr = user_table.query.filter_by(user_name = uname,
                                       pwd=pwd).first()
         if usr != None and usr.role == 0:
-            pass
+            return render_template("admin-dashboard.html")
+        elif usr and usr.role == 1:
+            return render_template("inf-dashboard.html",influencer = usr.user_name)
+        else:
+            return render_template("login.html",msg ="Invalid Credentials")    
+
+    return render_template("login.html",msg ="")
+
+@app.route("/inf_register",methods=["GET","POST"])
+def inf_signup():
+    if request.method == "POST":
+        uname = request.form.get("uname")
+        email = request.form.get("email")
+        fname = request.form.get("fname")
+        pwd =  request.form.get("pwd")
+        usr = user_table.query.filter_by(user_name = uname).first()
+        if not usr:
+            new_user = user_table(user_name = uname,email = email,full_name = fname,pwd = pwd,role = 1)
+            db.session.add(new_user)
+            db.session.commit()
             
+            # adding into influencer table
+            # id is the foreign key
+            category = request.form.get("category")
+            niche = request.form.get("niche")
+            followers_count = request.form.get("followers_count")
+            
+            new_infuencer = influencers(id = new_user.id,category = category,niche = niche,followers_count = followers_count)
+            db.session.add(new_infuencer)
+            db.session.commit()
 
-    return render_template("login.html")
+            return render_template("inf-signup.html",msg = "account created successfully. Kindly login!!!")
+            
+        else:
+            return render_template("inf-signup.html",msg = "user is already existing . kindly sign in")
+    return render_template("inf-signup.html",msg= "")
+        
 
-@app.route("/register",methods=["GET","POST"])
-def user_signup():
-    return render_template("sign_up.html")
+
+    
+
+@app.route("/spo_register",methods=["GET","POST"])
+def spo_signup():
+    if request.method == "POST":
+            uname = request.form.get("uname")
+            email = request.form.get("email")
+            fname = request.form.get("fname")
+            pwd =  request.form.get("pwd")
+            usr = user_table.query.filter_by(user_name = uname).first()
+            if not usr:
+                    new_user = user_table(user_name = uname,email = email,full_name = fname,pwd = pwd,role = 2)
+                    db.session.add(new_user)
+                    db.session.commit()
+                    
+                    # adding into sponsor table
+                    # id is the foreign key
+                    industry = request.form.get("industry")
+                    budget = request.form.get("budget")
+                    
+                    new_sponsor = sponsors(id = new_user.id,industry = industry,budget = budget)
+                    db.session.add(new_sponsor)
+                    db.session.commit()
+
+                    return render_template("spo-signup.html",msg = "account created successfully. Kindly login!!!")
+                    
+            else:
+                    return render_template("spo-signup.html",msg = "user is already existing . kindly sign in")
+    return render_template("spo-signup.html",msg = "")
--- backend/models.py
+++ backend/models.py
@@ -1,12 +1,32 @@
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy() #Instance of SQLAlchemy
+db = SQLAlchemy()  # Instance of SQLAlchemy
 
-class table_1(db.Model):
-    __tablename__ = "table_1"
-    id = db.Column(db.Integer,primary_key=True)
-    email = db.Column(db.String,nullable=False)
-    full_name=db.Column(db.String,nullable=False)
-    user_name=db.Column(db.String,nullable=False)
-    pwd=db.Column(db.String,nullable=False)
-    role=db.Column(db.Integer,nullable=False,default=1)
+class user_table(db.Model):
+    __tablename__ = "user_table"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String, nullable=False)
+    full_name = db.Column(db.String, nullable=False)
+    user_name = db.Column(db.String, nullable=False)
+    pwd = db.Column(db.String, nullable=False)
+    role = db.Column(db.Integer, nullable=False, default=1)
+    
+    
+
+class influencers(db.Model):
+    __tablename__ = "influencers"
+    id = db.Column(db.Integer, db.ForeignKey('user_table.id'), primary_key=True)
+    category = db.Column(db.String, nullable=False)
+    niche = db.Column(db.String, nullable=False)
+    followers_count = db.Column(db.Integer, nullable=False)
+
+class sponsors(db.Model):
+    __tablename__ = "sponsors"
+    id = db.Column(db.Integer, db.ForeignKey('user_table.id'), primary_key=True)
+    industry = db.Column(db.String, nullable=False)
+    budget = db.Column(db.Integer, nullable=False)
+
+# Relationship to Influencers and Sponsors
+user_influencer = db.relationship("Influencers", backref="user")
+user_sponsor = db.relationship("Sponsors", backref="user")
+
